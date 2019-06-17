4a520ef5df974043a01ac0a9a0c2570957359d2c
purich purisinsits <yuyuisa@192-168-1-13.tpgi.com.au>
2019-06-17 10:08:52 +1000

    chat page update

--- src/app/pages/chats/chats.page.ts
+++ src/app/pages/chats/chats.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ChatService } from 'src/app/services/chat.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chats',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatsPage implements OnInit {
 
-  constructor() { }
+  groups: Observable<any>;
+
+  constructor(
+    private authService: AuthService,
+    private chatService: ChatService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.groups = this.chatService.getGroups(); 
   }
+
+
 
 }
